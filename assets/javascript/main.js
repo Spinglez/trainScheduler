07434e01d8a10fe0b4582b371004c73f21fd7b90
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-10-28 20:25:30 -0700

    linked moment.js and firebase, fixed input id's

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,18 @@
-console.log("Linked JS");
+
+const config = {
+  apiKey: "AIzaSyC-MssCY30HLQ-LbrgrRIIrCKhQFVzx4Cc",
+  authDomain: "trainschedulers-a42f8.firebaseapp.com",
+  databaseURL: "https://trainschedulers-a42f8.firebaseio.com",
+  projectId: "trainschedulers-a42f8",
+  storageBucket: "trainschedulers-a42f8.appspot.com",
+  messagingSenderId: "631098993119"
+};
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+let train = {
+  name: "",
+  dest: "",
+  freq: null,
+}
