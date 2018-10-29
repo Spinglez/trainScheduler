f0974bceb2ea4b2b1bc0248c9afded4c53000db5
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-10-28 20:53:03 -0700

    added firebase appending to table data, working on calculations next.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,5 +14,34 @@ const database = firebase.database();
 let train = {
   name: "",
   dest: "",
+  trainT: null,
   freq: null,
 }
+
+function addTrainData(){
+  console.log("completed");
+  train.name = $('#trainInput').val().trim();
+  train.dest = $('#destInput').val().trim();
+  train.trainT = $('#trainTinput').val().trim();
+  train.freq = $('#freqInput').val().trim();
+
+  database.ref().push({
+    trainData: train,
+  })
+}
+
+$('#submit').on('click', function(event){
+  console.log("I'm clicked and calling addTrainData");
+  event.preventDefault();
+  addTrainData();
+})
+
+database.ref().on('child_added', function(childSnapshot){
+  console.log("test data pull", JSON.stringify(childSnapshot.val().trainData));
+  let row = $('<tr>');
+  $('.table').append(row);
+  $(row).append('<td>' + childSnapshot.val().trainData.name + '</td>')
+  $(row).append('<td>' + childSnapshot.val().trainData.dest + '</td>')
+  $(row).append('<td>' + childSnapshot.val().trainData.freq + '</td>')
+  $(row).append('<td>' + childSnapshot.val().trainData.trainT + '</td>')
+})
